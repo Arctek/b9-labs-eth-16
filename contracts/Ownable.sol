c90fea11bc48d337cd23e53ed33b7e1f8fd96a72
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 15:01:54 +1030

    Create Ownable contract. What is best here for Event/Function names? i.e. setOwner, transferOwner, updateOwner; are all similar. Same with modifiers, should these be is<State> or only<State>, has<State>?

---
+++ contracts/Ownable.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.4;
+
+contract Ownable {
+	address public owner;
+
+	event LogSetOwner(address newOwner);
+
+	modifier isOwner(){
+      require(msg.sender == owner);
+      _;
+	}
+
+	function Ownable public() {
+		owner = msg.sender;
+	}
+
+	function setOwner(address newOwner) isOwner public returns(bool success) {
+		require(newOwner != address(0));
+		LogSetOwner(newOwner);
+		owner = newOwner;
+		return true;
+	}
+}
