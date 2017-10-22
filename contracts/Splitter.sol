8b71c05667ae4f33c5bc747b41740ec3e459c1e6
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 15:33:29 +1030

    Splitter Contract skeleton. Perhaps we would like to log when a split transaction has a remainder. Also what should we do with the remainder, we could send it back now; store it against the sender recipient; or simply give it away to one of the receipient. While I'm here, should Events be named Log<Function>Event? Or is just Log<Function> fine.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,5 +3,27 @@ pragma solidity ^0.4.4;
 // TODO: Everything
 
 contract Splitter {
+	mapping(address => uint) public recipientbalances;
 
+	event LogSplit(address sender, address recipient1, address receipient2, bool hasRemainder);
+	event LogWithdraw(address recipient, uint withdrawAmount);
+
+	function split(address recipient1, address recipient2) 
+		isNotPaused
+		isNotKilled
+		payable
+		public
+		returns(bool success)
+	{
+
+	}
+
+	function withdraw(uint withdrawAmount)
+		isNotPaused
+		isNotKilled
+		public
+		returns(bool success)
+	{
+
+	}
 }
