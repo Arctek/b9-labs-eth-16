e7a70b6597c3bb365580a9756db02a21790ae89a
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 17:19:48 +1030

    Created tests for Ownable. Async/await are useful here testing for invalid conditions.

--- test/ownable.js
+++ test/ownable.js
@@ -1,20 +1,38 @@
+'use strict';
+const assertInvalidOpcode = require('../test_util/assertInvalidOpcode');
+
 var Ownable = artifacts.require("./Ownable.sol");
 
 contract('Ownable', function(accounts) {
     var contract;
 
-    var owner   = accounts[0];
-    var bob     = accounts[1];
+    const owner   = accounts[0];
+    const bob     = accounts[1];
 
     beforeEach(function() {
         return Ownable.new({ from: owner }).then(instance => contract = instance);
     });
 
-    it('should not allow non-owner to set owner', () => {
+    it('should not allow non-owner to set owner', async function() {
+        try {
+            await contract.setOwner(bob, {from: bob});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
     it('should allow owner to set owner', () => {
+        return contract.setOwner(bob, { from: owner }
+        ).then(() => {
+            return contract.owner();
+        })
+        .then(newOwner => {
+            assert.strictEqual(newOwner, bob, "the owner was not changed")
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
-
 
 });
