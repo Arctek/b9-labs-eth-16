a07e83a68653a75ba4a424ed60c663041f5f3436
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 17:23:34 +1030

    Mostly the same test structure for Pausable contract. I wonder if there is a nice way to generalise for checking other contracts in future that have similar variables/set functions.

--- test/pauseable.js
+++ test/pauseable.js
@@ -1,19 +1,37 @@
+'use strict';
+const assertInvalidOpcode = require('../test_util/assertInvalidOpcode');
+
 var Pauseable = artifacts.require("./Pauseable.sol");
 
 contract('Pauseable', function(accounts) {
     var contract;
 
-    var owner   = accounts[0];
-    var bob     = accounts[1];
+    const owner   = accounts[0];
+    const bob     = accounts[1];
 
     beforeEach(function() {
         return Pauseable.new({ from: owner }).then(instance => contract = instance);
     });
 
-    it('should not allow non-owner to pause', () => {
+    it('should not allow non-owner to pause', async function() {
+        try {
+            await contract.setPaused(true, {from: bob});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
     it('should allow owner to pause', () => {
+            return contract.setPaused(true, { from: owner }
+        ).then(() => {
+            return contract.paused();
+        })
+        .then(newPaused => {
+            assert.strictEqual(newPaused, true, "paused was not changed")
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
-
 });
