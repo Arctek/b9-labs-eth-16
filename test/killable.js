da0d3ebd1bb2181abc68bf84b16550e93d6c259c
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 17:50:55 +1030

    Killable contract tests. What should I be checking for on functions that send ether? At the moment I am checking the return value (bool success), but I could also check the logged event and the transaction itself. Otherwise I could check all three.

--- test/killable.js
+++ test/killable.js
@@ -1,28 +1,76 @@
+'use strict';
+const assertInvalidOpcode = require('../test_util/assertInvalidOpcode');
+
 var Killable = artifacts.require("./Killable.sol");
 
 contract('Killable', function(accounts) {
     var contract;
 
-    var owner   = accounts[0];
-    var bob     = accounts[1];
+    const owner   = accounts[0];
+    const bob     = accounts[1];
 
     beforeEach(function() {
         return Killable.new({ from: owner }).then(instance => contract = instance);
     });
 
-    it('should not allow non-owner to kill the contract', () => {
+    it('should not allow non-owner to kill the contract', async function() {
+        try {
+            await contract.kill({from: bob});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
     it('should allow owner to kill the contract', () => {
+            return contract.kill({ from: owner }
+        ).then(() => {
+            return contract.killed();
+        })
+        .then(newKilled => {
+            assert.strictEqual(newKilled, true, "the contract was not killed")
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
 
-    it('should not allow the owner to emergency withdraw on a unkilled contract', () => {
+    it('should not allow the owner to emergency withdraw on a unkilled contract', async function() {
+        try {
+            await contract.emergencyWithdrawal({from: owner});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
-    it('should not allow non-owner to emergency withdraw', () => {
+    it('should not allow non-owner to emergency withdraw a killed contract', () => {
+            return contract.kill({ from: owner }
+        )
+        .then( async function() {
+            try {
+                await contract.emergencyWithdrawal({from: bob});
+                assert.fail('should have failed');
+            } catch(error) {
+                assertInvalidOpcode(error);
+            }
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
 
-    it('should allow owner to emergency withdraw', () => {
+    it('should allow owner to emergency withdraw a killed contract', () => {
+            return contract.kill({ from: owner }
+        ).then(() => {
+            return contract.emergencyWithdrawal.call({ from: owner });
+        })
+        .then(success => {
+            assert.strictEqual(success, true, "the owner could not emergency withdraw")
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
 
 
