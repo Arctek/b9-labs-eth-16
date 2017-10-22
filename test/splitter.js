4e5d70c79e10a90dd0df3875e9937c683e47f984
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 19:27:25 +1030

    Some tests for Splitter

--- test/splitter.js
+++ test/splitter.js
@@ -1,13 +1,17 @@
+'use strict';
+const assertInvalidOpcode = require('../test_util/assertInvalidOpcode');
+
 var Splitter = artifacts.require("./Splitter.sol");
 
 contract('Splitter', function(accounts) {
     var contract;
 
-    var owner   = accounts[0];
-    var bob     = accounts[1];
-    var carol   = accounts[2];
-    var david   = accounts[3];
-    var emma    = accounts[4];
+    const evenContribution = 2200;
+    const oddContribution  = 2201;
+
+    const owner   = accounts[0];
+    const bob     = accounts[1];
+    const carol   = accounts[2];
 
     beforeEach(function() {
         return Splitter.new({ from: owner }).then(instance => contract = instance);
@@ -15,21 +19,83 @@ contract('Splitter', function(accounts) {
 
     // Split checks
     it('should not allow split on a paused contract', () => {
+            return contract.setPaused(true, { from: owner }
+        )
+        .then( async function() {
+            try {
+                await contract.split(bob, carol, {from: owner, value: evenContribution});
+                assert.fail('should have failed');
+            } catch(error) {
+                assertInvalidOpcode(error);
+            }
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
    
     it('should not allow split on a killed contract', () => {
+            return contract.kill({ from: owner }
+        )
+        .then( async function() {
+            try {
+                await contract.split(bob, carol, {from: owner, value: evenContribution});
+                assert.fail('should have failed');
+            } catch(error) {
+                assertInvalidOpcode(error);
+            }
+        })
+        .catch(err => {
+            assert.fail(err)
+        });
     });
 
-    it('should not allow receipient1 to be null', () => {
+    it('should not allow receipient1 to be null', async function() {
+        try {
+            await contract.split(null, carol, {from: owner, value: evenContribution});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
-    it('should not allow receipient2 to be null', () => {
+    it('should not allow receipient2 to be null', async function() {
+        try {
+            await contract.split(bob, null, {from: owner, value: evenContribution});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
-    it('should not allow zero split amounts', () => {
+    it('should not allow receipient1 to be the same as receipient2', async function() {
+        try {
+            await contract.split(bob, bob, {from: owner, value: evenContribution});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
     });
 
-    it('should split to the two receipients', () => {
+    it('should not allow zero split amounts', async function() {
+        try {
+            await contract.split(bob, carol, {from: owner, value: 0});
+            assert.fail('should have failed');
+        } catch(error) {
+            assertInvalidOpcode(error);
+        }
+    });
+
+    it('should split to the two receipients', async function() {
+        let splitTx = await contract.split(bob, carol, {from: owner, value: evenContribution} );
+
+        let bobBalance = await contract.recipientBalances.call(bob);
+
+        assert.isAbove(bobBalance.toNumber(), 0, "the funds were not split");
+
+        let carolBalance = await contract.recipientBalances.call(bob);
+
+        assert.isAbove(carolBalance.toNumber(), 0, "the funds were not split");        
     });
 
     it('should send the remainder amounts to the sender', () => {
