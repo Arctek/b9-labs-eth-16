befde218a74a27c9689a3a46ee4645985eb865de
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 15:40:30 +1030

    Fix typos. Filled in split function. Thanks Rob: https://stackoverflow.com/questions/42738640/division-in-ethereum-solidity With functions is it better to define local variables at the top, before any require?

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.4;
 // TODO: Everything
 
 contract Splitter {
-	mapping(address => uint) public recipientbalances;
+	mapping(address => uint) public recipientBalances;
 
 	event LogSplit(address sender, address recipient1, address receipient2, bool hasRemainder);
 	event LogWithdraw(address recipient, uint withdrawAmount);
@@ -15,10 +15,34 @@ contract Splitter {
 		public
 		returns(bool success)
 	{
-		require(receipient1 != address(0));
-		require(receipient2 != address(0));
-		require(receipient1 != receipient2);
+		uint quotient;
+		uint remainder;
+		bool hasRemainder;
+
+		require(recipient1 != address(0));
+		require(recipient2 != address(0));
+		require(recipient1 != recipient2);
 		require(msg.value > 0);
+
+		quotient  = msg.value / 2;
+        remainder = msg.value - 2 * quotient;
+
+        require(quotient > 0);
+
+        if (remainder > 0) {
+        	hasRemainder = true;
+        }
+
+        LogSplit(msg.sender, recipient1, recipient2)
+
+        recipientBalances[recipient1] += quotient;
+        recipientBalances[recipient2] += quotient;
+
+        if (hasRemainder) {
+        	recipientBalances[msg.sender] += remainder;
+        }
+
+        return true;
 	}
 
 	function withdraw(uint withdrawAmount)
