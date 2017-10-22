0fdde525603b527971b229fb80952b32246e5a58
Joshua Richardson <joshua@webcoders.com.au>
2017-10-22 15:13:19 +1030

    Create Pausable contract, inherits Ownable. Owner should be able to pause and unpause a Contract.

---
+++ contracts/Pauseable.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.4;
+
+import "./Ownable.sol";
+
+contract Pauseable is Ownable{
+	bool public paused;
+
+	event LogSetPaused(bool paused);
+
+	modifier isPaused(){
+      require(paused);
+      _;
+	}
+
+	modifier isNotPaused(){
+      require(!paused);
+      _;
+	}
+
+	function setPaused(bool newPaused) isOwner public returns(bool success) {
+		LogSetOwner(newPaused);
+		paused = newPaused;
+		return true;
+	}
+}
