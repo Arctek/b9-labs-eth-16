fcafcbdc49293543fb19e25ef9b25c0652de2280
Joshua Richardson <joshua@webcoders.com.au>
2017-10-25 01:08:45 +1030

    Updated Paused contract tests. Making use of Xavier's helpful extensions here to ensure we have unlocked accounts. Added default (unpaused) test case.

--- test/pauseable.js
+++ test/pauseable.js
@@ -1,22 +1,41 @@
 'use strict';
-const expectedExceptionPromise = require("../test_util/expected_exception_testRPC_and_geth.js");
 
-var Pauseable = artifacts.require("./Pauseable.sol");
+const Pauseable = artifacts.require("./Pauseable.sol");
+
+import { default as Promise } from 'bluebird';
+
+if (typeof web3.eth.getBlockPromise !== "function") {
+    Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+}
+
+web3.eth.getTransactionReceiptMined = require("../test_util/getTransactionReceiptMined.js");
+web3.eth.expectedPayableExceptionPromise = require("../test_util/expectedPayableExceptionPromise.js");
+web3.eth.expectedExceptionPromise = require("../test_util/expectedExceptionPromise.js");
+web3.eth.makeSureAreUnlocked = require("../test_util/makeSureAreUnlocked.js");
+web3.eth.makeSureHasAtLeast = require("../test_util/makeSureHasAtLeast.js");
 
 contract('Pauseable', accounts => {
-    let contract;
-
-    const owner   = accounts[0];
-    const bob     = accounts[1];
-
     const gasToUse = 3000000;
+    let owner, bob;
+
+    before("should prepare accounts", function() {
+        assert.isAtLeast(accounts.length, 2, "should have at least 2 accounts");
+        owner = accounts[0];
+        bob = accounts[1];
+        return web3.eth.makeSureAreUnlocked([owner, bob]);
+    });
 
     beforeEach(() => {
         return Pauseable.new({ from: owner }).then(instance => contract = instance);
     });
 
+    it('should be initialized as unpaused', () => {
+        return contract.paused().then(isPaused => { assert.strictEqual(isPaused, false, "paused");
+        });
+    });
+
     it('should not allow non-owner to pause', () => {
-        return expectedExceptionPromise(() => {
+        return web3.eth.expectedExceptionPromise(() => {
             return contract.setPaused(true, { from: bob, gas: gasToUse });
         }, gasToUse);
     });
@@ -26,8 +45,8 @@ contract('Pauseable', accounts => {
         ).then(() => {
             return contract.paused();
         })
-        .then(newPaused => {
-            assert.strictEqual(newPaused, true, "paused was not changed");
+        .then(isPaused => {
+            assert.strictEqual(isPaused, true, "paused was not changed");
         })
         .catch(err => {
             assert.fail(err);
