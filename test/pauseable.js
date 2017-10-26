4c3a12da2a9b99253cf17e3b62eb18944372c3be
Joshua Richardson <joshua@webcoders.com.au>
2017-10-26 23:38:15 +1030

    More cleanup here.

--- test/pauseable.js
+++ test/pauseable.js
@@ -13,6 +13,9 @@ web3.eth.expectedPayableExceptionPromise = require("../test_util/expectedPayable
 web3.eth.expectedExceptionPromise = require("../test_util/expectedExceptionPromise.js");
 web3.eth.makeSureAreUnlocked = require("../test_util/makeSureAreUnlocked.js");
 web3.eth.makeSureHasAtLeast = require("../test_util/makeSureHasAtLeast.js");
+web3.eth.sequentialPromise = require("../test_util/sequentialPromise.js");
+assert.topicContainsAddress = require("../test_util/topicContainsAddress.js");
+assert.topicContainsBoolean = require("../test_util/topicContainsBoolean.js");
 
 contract('Pauseable', accounts => {
     const gasToUse = 3000000;
@@ -35,50 +38,51 @@ contract('Pauseable', accounts => {
     });
 
     it('should not allow non-owner to pause', () => {
-        return web3.eth.expectedExceptionPromise(() => {
-            return contract.setPaused(true, { from: bob, gas: gasToUse });
-        }, gasToUse);
+        return web3.eth.expectedExceptionPromise(() =>
+            contract.setPaused(true, { from: bob, gas: gasToUse }),
+        gasToUse);
     });
 
-    it('should allow owner to pause', () => {
-            return contract.setPaused(true, { from: owner }
-        ).then(txObject => {
-            asertEventLogSetPause(txObject, owner, true);
+    it('should allow owner to pause', async () => {
+        let txObject;
 
-            return contract.paused();
-        })
-        .then(isPaused => {
-            assert.strictEqual(isPaused, true, "paused was not changed");
-        });
+        let results = await web3.eth.sequentialPromise([
+            () => contract.setPaused(true, { from: owner }).then(tx => txObject = tx),
+            () => contract.paused()
+        ]);
+
+        assertEventLogSetPause(txObject, owner, true);
+
+        assert.isTrue(results[1], true, "paused was not changed");
     });
 
-    it('should not allow owner to change paused to the same value', () => {
-            return contract.setPaused(true, { from: owner }
-        ).then(() => {
-            return web3.eth.expectedExceptionPromise(() => {
-                return contract.setPaused(true, { from: owner, gas: gasToUse });
-            }, gasToUse);
-        });
+    it('should not allow owner to change paused to the same value', async () => {
+        await web3.eth.sequentialPromise([
+            () => contract.setPaused(true, { from: owner }),
+            () => web3.eth.expectedExceptionPromise(() => 
+                    contract.setPaused(true, { from: owner, gas: gasToUse }), gasToUse)
+        ]);
     });
 
-    it('should allow owner to unpause', () => {
-            return contract.setPaused(true, { from: owner }
-        ).then(txObject => {
-            asertEventLogSetPause(txObject, owner, true);
+    it('should allow owner to unpause', async () => {
+        let txObject;
 
-            return contract.setPaused(false, { from: owner });
-        }).then(txObject => {
-            asertEventLogSetPause(txObject, owner, false);
+        let results = await web3.eth.sequentialPromise([
+            () => contract.setPaused(true, { from: owner }).then(tx => txObject = tx),
+            () => contract.paused(),
+            () => contract.setPaused(false, { from: owner }).then(tx => txObject = tx),
+            () => contract.paused()
+        ]);
 
-            return contract.paused();
-        })
-        .then(isPaused => {
-            assert.strictEqual(isPaused, false, "paused was not changed");
-        });
+        assertEventLogSetPause(results[0], owner, true);
+        assertEventLogSetPause(results[2], owner, false);
+
+        assert.isTrue(results[1], true, "paused was not changed");
+        assert.isFalse(results[3], false, "paused was not changed");
     });
 });
 
-function asertEventLogSetPause(txObject, who, paused) {
+function assertEventLogSetPause(txObject, who, paused) {
     assert.equal(txObject.logs.length, 1, "should have received 1 event");
     assert.strictEqual(txObject.logs[0].event, "LogSetPaused", "should have received LogSetPaused event");
             
@@ -90,33 +94,10 @@ function asertEventLogSetPause(txObject, who, paused) {
         txObject.logs[0].args.paused,
         paused,
         "should be the new paused value");
-    // who and paused should be indexed
-    assert.equal(txObject.receipt.logs[0].topics.length, 3, "should have 3 topics");
+    
+    assert.strictEqual(txObject.receipt.logs[0].topics.length, 3, "should have 3 topics");
 
-    assertTopicContainsAddress(txObject.receipt.logs[0].topics[1], who);
-    assertTopicContainsBoolean(txObject.receipt.logs[0].topics[2], paused);
-
-}
-
-function assertTopicContainsAddress(topic, address) {
-    assert.strictEqual(address.length, 42, "should be 42 characters long");
-    assert.strictEqual(topic.length, 66, "should be 64 characters long");
-
-    address = "0x" + address.substring(2).padStart(64, "0");
-
-    assert.strictEqual(topic, address, "topic should match address");
-}
-
-function assertTopicContainsBoolean(topic, boolToCheck) {
-    assert.strictEqual(topic.length, 66, "should be 64 characters long");
-
-    assert.strictEqual(boolToCheck === true || boolToCheck === false, true, "not a boolean");
-
-    if (boolToCheck === true) {
-        assert.strictEqual(topic, "0x0000000000000000000000000000000000000000000000000000000000000001", "topic does not match true");
-    }
-    else {
-        assert.strictEqual(topic, "0x0000000000000000000000000000000000000000000000000000000000000000", "topic does not match false");
-    }
+    assert.topicContainsAddress(txObject.receipt.logs[0].topics[1], who);
+    assert.topicContainsBoolean(txObject.receipt.logs[0].topics[2], paused);
 
 }
